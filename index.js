feb75d771e5a48e372aa5040b345814fe289081d
unknown <abhishekbohara97@gmail.com>
2021-01-27 15:12:26 +0530

    updating index.js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello Abhishek Bohara!')
 })
 
 module.exports.handler = serverless(app);
